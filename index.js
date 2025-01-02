74ecad51f85b19c21e1a36d45d23054197b221b7
Kat <kat.m.deans@gmail.com>
2025-01-02 15:44:59 -0500

    Fetching data from API

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function openMenu() {
-  document.body.classList += " menu--open"
+// https://www.omdbapi.com/?apikey=aecc1b78&s=fast
+
+async function main() {
+  const data = await fetch("https://www.omdbapi.com/?apikey=aecc1b78&s=fast")
+  const movieData = await data.json();
+  const result = movieData.Search
+
+  console.log(result)
 }
 
-function closeMenu() {
-  document.body.classList.remove('menu--open')
-}
+main()
+
+// we want to give it to the search button, so we will use event listener, and it will be onclick inside of the html, for now I can the function directly in js using main() 
+// after we remoe the main function and give it to the button search 
+
+
